46f854ef73ee567702c637919dd933ff610f7b7c
disha <dishanayak395@gmail.com>
2026-02-19 09:34:48 +0530

    Configure ModelMapper bean for DTO mapping

--- backend/ims-backend/src/main/java/com/example/ims_backend/services/impl/CategoryServiceImpl.java
+++ backend/ims-backend/src/main/java/com/example/ims_backend/services/impl/CategoryServiceImpl.java
@@ -1,4 +1,62 @@
 package com.example.ims_backend.services.impl;
 
-public class CategoryServiceImpl {
+import com.example.ims_backend.dto.CategoryDto;
+import com.example.ims_backend.dto.Response;
+import com.example.ims_backend.repository.CategoryRepository;
+import com.example.ims_backend.services.ICategoryService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class CategoryServiceImpl implements ICategoryService {
+    private final CategoryRepository categoryRepository;
+
+
+    /**
+     * @param categoryDto
+     * @return
+     */
+    @Override
+    public Response createCategory(CategoryDto categoryDto) {
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    @Override
+    public Response getAllCategories() {
+        return null;
+    }
+
+    /**
+     * @param id
+     * @return
+     */
+    @Override
+    public Response getCategoryById(Long id) {
+        return null;
+    }
+
+    /**
+     * @param id
+     * @param categoryDto
+     * @return
+     */
+    @Override
+    public Response updateCategory(Long id, CategoryDto categoryDto) {
+        return null;
+    }
+
+    /**
+     * @param id
+     * @return
+     */
+    @Override
+    public Response deleteCategory(Long id) {
+        return null;
+    }
 }
